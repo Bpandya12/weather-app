d2fc83f583542a6f97ddf48f62a2c0475badb5c0
Bejal <b.pandya12@gmail.com>
2020-11-04 18:21:20 -0500

    Updated Icon Selector

--- index.js
+++ index.js
@@ -49,6 +49,12 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#cur-weather").innerHTML =
     response.data.weather[0].description;
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/01n@2x.png`
+  );
 }
 
 function search(city) {
